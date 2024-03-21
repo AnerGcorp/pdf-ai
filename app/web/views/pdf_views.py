041ef00aa7d4cd00e83e955f313f075bc1aa44f0
anergcorp <ahunatajanov@gmail.com>
2024-03-22 01:26:36 +0200

    created celery job for processing

--- app/web/views/pdf_views.py
+++ app/web/views/pdf_views.py
@@ -26,8 +26,7 @@ def upload_file(file_id, file_path, file_name):
 
     pdf = Pdf.create(id=file_id, name=file_name, user_id=g.user.id)
 
-    # TODO: Defer this to be processed by the worker
-    process_document(pdf.id)
+    process_document.delay(pdf.id)
 
     return pdf.as_dict()
 
