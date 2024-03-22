c4262f15aad69bdfe856a959e330ed3f59e1a11f
anergcorp <ahunatajanov@gmail.com>
2024-03-23 01:10:09 +0200

    streaming with callbacks

--- test.py
+++ test.py
@@ -1,11 +1,21 @@
+from typing import Any
+from uuid import UUID
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.chains import LLMChain
+from langchain.callbacks.base import BaseCallbackHandler
 from dotenv import load_dotenv
 
 load_dotenv()
 
-chat = ChatOpenAI(streaming=True)
+class StreamingHandler(BaseCallbackHandler):
+    def on_llm_new_token(self, token: str, **kwargs: Any) -> Any:
+        print(token)
+
+chat = ChatOpenAI(
+    streaming=True,
+    callbacks=[StreamingHandler()]
+)
 
 prompt = ChatPromptTemplate.from_messages([
     ("human", "{content}")
