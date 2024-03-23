babcc13dd3df254681f48aa7ec3161ca578332ea
anergcorp <ahunatajanov@gmail.com>
2024-03-23 05:51:02 +0200

    implemented filtering between chat models when doing streaming and not

--- app/chat/callbacks/stream.py
+++ app/chat/callbacks/stream.py
@@ -1,16 +1,24 @@
-from typing import Any
+from typing import Any, Dict, List
+from uuid import UUID
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain.schema.output import LLMResult
 
 class StreamingHandler(BaseCallbackHandler):
     def __init__(self, queue):
         self.queue = queue
+        self.streaming_run_ids = set()
+
+    def on_llm_start(self, serialized, prompts, run_id, **kwargs: Any) -> Any:
+        if serialized['kwargs']['streaming']:
+            self.streaming_run_ids.add(run_id)
 
     def on_llm_new_token(self, token: str, **kwargs: Any) -> Any:
         self.queue.put(token)
     
-    def on_llm_end(self, response: LLMResult, **kwargs: Any) -> Any:
-        self.queue.put(None)
+    def on_llm_end(self, response: LLMResult, run_id, **kwargs: Any) -> Any:
+        if run_id in self.streaming_run_ids:
+            self.queue.put(None)
+            self.streaming_run_ids.remove(run_id)
     
     def on_llm_error(self, error, **kwargs: Any) -> Any:
         self.queue.put(None)
