5feabe0feb8bfd0d94b521789bfedbf29e3a2bfb
anergcorp <ahunatajanov@gmail.com>
2024-03-23 05:45:57 +0200

    implemented filtering between chat models when doing streaming and not

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -1,3 +1,4 @@
+from langchain.chat_models import ChatOpenAI
 from app.chat.models import ChatArgs
 from app.chat.vector_stores.pinecone import build_retriever
 from app.chat.llms.chatopenai import build_llm
@@ -17,10 +18,12 @@ def build_chat(chat_args: ChatArgs):
     """
     retriever = build_retriever(chat_args=chat_args)
     llm = build_llm(chat_args=chat_args)
+    condense_question_llm = ChatOpenAI(streaming=False)
     memory = build_memory(chat_args=chat_args)
 
     return StreamingConversationalRetrievalChain.from_llm(
         llm=llm,
+        condense_question_llm=condense_question_llm,
         memory=memory,
         retriever=retriever
     )
