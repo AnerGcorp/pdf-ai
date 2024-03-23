cf6cbc6e6f643ddeeeb5780d501523acf61272a2
anergcorp <ahunatajanov@gmail.com>
2024-03-23 23:08:27 +0200

    creating window buffer memory

--- app/chat/memories/__init__.py
+++ app/chat/memories/__init__.py
@@ -1,5 +1,7 @@
 from .sql_memory import build_memory
+from .window_memory import window_buffer_memory_builder
 
 memory_map = {
-    "sql_buffer_memory": build_memory
+    "sql_buffer_memory": build_memory,
+    "sql_window_memory": window_buffer_memory_builder
 }
--- app/chat/memories/sql_memory.py
+++ app/chat/memories/sql_memory.py
@@ -1,29 +1,9 @@
-from langchain.schema.messages import BaseMessage
-from pydantic import BaseModel
+
 from langchain.memory import ConversationBufferMemory
-from langchain.schema import BaseChatMessageHistory
+from app.chat.memories.histories.sql_history import SqlMessageHistory
 
-from app.web.api import (
-    get_messages_by_conversation_id,
-    add_message_to_conversation
-)
 
-class SqlMessageHistory(BaseChatMessageHistory, BaseModel):
-    conversation_id: str
 
-    @property
-    def messages(self):
-        return get_messages_by_conversation_id(self.conversation_id)
-    
-    def add_message(self, message: BaseMessage) -> None:
-        return add_message_to_conversation(
-            conversation_id=self.conversation_id,
-            role=message.type,
-            content=message.content
-        )
-    
-    def clear(self) -> None:
-        pass
 
 def build_memory(chat_args):
     return ConversationBufferMemory(
